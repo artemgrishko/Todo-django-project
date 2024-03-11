c8a801ef7c04ebb46b3e69ad6d0f56374ce45f0a
Artem Hryshko <artemggg2222@gmail.com>
2024-03-11 12:52:41 +0200

    add url, view and template for deleting tags

--- todo/views.py
+++ todo/views.py
@@ -22,3 +22,8 @@ class TagUpdateView(generic.UpdateView):
     model = Tag
     fields = "__all__"
     success_url = reverse_lazy("todo:tag-list")
+
+
+class TagDeleteView(generic.DeleteView):
+    model = Tag
+    success_url = reverse_lazy("todo:tag-list")
