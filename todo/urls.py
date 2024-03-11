83873addf4a8843acba129dcfd8af668c6ac6b07
Artem Hryshko <artemggg2222@gmail.com>
2024-03-11 11:46:13 +0200

    add url, view and template for Tag

--- todo/urls.py
+++ todo/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from todo.views import TaskListView
+from todo.views import TaskListView, TagListView
 
 urlpatterns = [
-    path("", TaskListView.as_view(), name="task_list")
+    path("", TaskListView.as_view(), name="task_list"),
+    path("tags/", TagListView.as_view(), name="tag_list")
 ]
 
 app_name = "todo"
