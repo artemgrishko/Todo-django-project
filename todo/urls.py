1bdb934b7b3cce02e8db5ae93719711c43ca6743
Artem Hryshko <artemggg2222@gmail.com>
2024-03-11 13:56:39 +0200

    update view and url for task_list

--- todo/urls.py
+++ todo/urls.py
@@ -1,14 +1,17 @@
 from django.urls import path
 from todo.views import (
-    TaskListView,
+    # TaskListView,
+    task_list,
     TagListView,
     TagCreateView,
     TagUpdateView,
     TagDeleteView,
+    TaskCreateView,
 )
 
 urlpatterns = [
-    path("", TaskListView.as_view(), name="task-list"),
+    path("", task_list, name="task-list"),
+    path("create/", TaskCreateView.as_view(), name="task-create"),
     path("tags/", TagListView.as_view(), name="tag-list"),
     path("tags/create/", TagCreateView.as_view(), name="tag-create"),
     path("tags/<int:pk>/update/", TagUpdateView.as_view(), name="tag-update"),
--- todo/views.py
+++ todo/views.py
@@ -1,11 +1,21 @@
+from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import generic
 
 from todo.models import Task, Tag
 
 
-class TaskListView(generic.ListView):
+def task_list(request):
+    tasks = Task.objects.all()
+    for task in tasks:
+        task.tag_list = ", ".join(tag.name for tag in task.tags.all())
+    return render(request, 'todo/task_list.html', {'task_list': tasks})
+
+
+class TaskCreateView(generic.CreateView):
     model = Task
+    fields = "__all__"
+    success_url = reverse_lazy("todo:task-list")
 
 
 class TagListView(generic.ListView):
